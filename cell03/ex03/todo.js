904b1c3a74d84d4abd72134db2310dce54c40b49
Sirikul Khummung <Impeniparaloy@gmail.com>
2025-02-05 01:29:23 +0700

    update js

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -4,39 +4,51 @@ let TodoList = []
 function createTodo(text) {
     const element = document.createElement('div')
     element.classList.add('todo')
-    element.innerHTML = '<p>' + text + '</p>' + '<button onclick="remove(' + "'" + text + "'" + ')">Delete</button>'
+    element.innerHTML = `<p>${text}</p><button onclick="remove('${text}')">Delete</button>`
     return element;
 }
 
 function render() {
     list.innerHTML = ''
-    for (let index = 0; index < TodoList.length; index++) {
-        const element = TodoList[index];
-        list.innerHTML += createTodo(element).outerHTML
+    TodoList.forEach(item => {
+        list.appendChild(createTodo(item))
+    })
+}
+
+function saveCookies() {
+    document.cookie = `todos=${encodeURIComponent(JSON.stringify(TodoList))}; path=/`
+}
+
+function loadCookies() {
+    const cookies = document.cookie.split('; ')
+    const todoCookie = cookies.find(row => row.startsWith('todos='))
+    if (todoCookie) {
+        try {
+            TodoList = JSON.parse(decodeURIComponent(todoCookie.split('=')[1]))
+        } catch (e) {
+            TodoList = []  // Reset if corrupted
+        }
     }
-    document.cookie = JSON.stringify(TodoList)
 }
 
 function newTodo() {
     let name = prompt("Name the todo.")
-    if (name.length > 0) {
-        TodoList.unshift(name)
+    if (name && name.trim().length > 0) {
+        TodoList.unshift(name.trim())
+        saveCookies()
         render()
     }
 }
 
 function remove(text) {
-    let yes = confirm('Are you sure to remove.')
-    if (yes) {
-        TodoList = TodoList.filter((a) => a != text)
+    if (confirm('Are you sure to remove?')) {
+        TodoList = TodoList.filter(item => item !== text)
+        saveCookies()
         render()
     }
 }
 
 window.onload = function () {
-    let save = document.cookie;
-    if (save.length > 0) {
-        TodoList = JSON.parse(save)
-        render()
-    }
+    loadCookies()
+    render()
 }
--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,47 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let TodoList = [];
 
     function createTodo(text) {
-        return $('<div>')
-            .addClass('todo')
-            .append(
-                $('<p>').text(text),
-                $('<button>')
-                    .text('Delete')
-                    .click(function() { remove(text); })
-            );
+        let $todo = $('<div class="todo"></div>')
+            .append(`<p>${text}</p>`)
+            .append('<button class="delete">Delete</button>')
+            .data('text', text);
+
+        return $todo;
     }
 
     function render() {
         $('#ft_list').empty();
-        TodoList.forEach(function(element) {
-            $('#ft_list').append(createTodo(element));
+        TodoList.forEach(item => {
+            $('#ft_list').append(createTodo(item));
         });
-        document.cookie = JSON.stringify(TodoList);
     }
 
-    function newTodo() {
+    function saveCookies() {
+        document.cookie = `todos=${encodeURIComponent(JSON.stringify(TodoList))}; path=/`;
+    }
+
+    function loadCookies() {
+        const cookies = document.cookie.split('; ');
+        const todoCookie = cookies.find(row => row.startsWith('todos='));
+        if (todoCookie) {
+            try {
+                TodoList = JSON.parse(decodeURIComponent(todoCookie.split('=')[1]));
+            } catch (e) {
+                TodoList = [];
+            }
+        }
+    }
+
+    $('#newTodo').on('click', function () {
         let name = prompt("Name the todo.");
-        if (name && name.length > 0) {
-            TodoList.unshift(name);
+        if (name && name.trim().length > 0) {
+            TodoList.unshift(name.trim());
+            saveCookies();
             render();
         }
-    }
+    });
 
-    function remove(text) {
-        if (confirm('Are you sure to remove.')) {
-            TodoList = TodoList.filter(a => a !== text);
+    $('#ft_list').on('click', '.delete', function () {
+        let text = $(this).parent().data('text');
+        if (confirm('Are you sure to remove?')) {
+            TodoList = TodoList.filter(item => item !== text);
+            saveCookies();
             render();
         }
-    }
+    });
 
-    // Event handler for new todo button
-    $('#new').click(newTodo);
-
-    // Load saved todos on page load
-    let save = document.cookie;
-    if (save.length > 0) {
-        TodoList = JSON.parse(save);
-        render();
-    }
+    // Initialize on page load
+    loadCookies();
+    render();
 });
